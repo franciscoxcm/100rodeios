f904e3966d4d8d9aaad6ab899fdfe7b888fc2f51
Francisco <franciscoxcm@live.com.pt>
2023-06-12 17:26:36 +0100

    12/06

--- home.php
+++ home.php
@@ -1,13 +1,39 @@
 <?php require_once("inicio.php") ?>
 <?php require_once("nav-bar.php") ?>
 
-<div class="uk-section uk-padding-remove uk-grid-collapse corpo" data-uk-grid>
-    <div class="uk-width-auto@m">
-        <img src="img/linha-sol.svg" alt="">
+<div class="uk-section uk-padding-remove">
+
+    <div class="uk-position-relative">
+        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: 7:5; animation: pull; autoplay: true;  autoplay-interval: 2000; velocity: 0.1">
+
+            <ul class="uk-slideshow-items">
+                <li>
+                <div class="uk-position-cover uk-animation-kenburns uk-animation-reverse uk-transform-origin-center-left">
+                        <img class="" src="img/hp/Slider/base-escura.png" alt="" uk-cover>
+                    </li>
+                <li>
+                    <div class="uk-position-cover uk-animation-kenburns uk-animation-reverse uk-transform-origin-center-left">
+                        <img src="img/hp/Slider/base-clara.png" alt="" uk-cover>
+                    </div>
+                </li>
+            </ul>
+
+            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+
+        </div>
+        <div class="uk-position-relative" uk-sticky="offset: 100">
+            <img class="onda-azul-topo uk-position-top" src="img/nav-bar/onda-topo-azul.png" alt="">
+        </div>
+    </div>
+
+    <div class="uk-width-auto@m" uk-sticky="offset: 200">
+        <img src="img/hp/linha-sol.svg" alt="">
     </div>
     <div class="uk-width-expand@m epocas-dia">
         <div class="manha">
             <h2>A nossa Manhâ</h2>
+            <img class="" src="img/hp/Slider/base-escura.png" alt="">
         </div>
         <div class="tarde">
             <h2>A nossa Tarde</h2>
--- nav-bar.php
+++ nav-bar.php
@@ -7,14 +7,14 @@
         <div class=""><img class="uk-position-absolute splash" src="img/nav-bar/onda-amarela-navbar2.png" alt=""></div>
         <div class="uk-flex uk-flex-between uk-flex-middle icons">
             <div id="menu" class="menu"><img src="img/nav-bar/menu.png" alt=""></div>
-            <div><a href="#"><img src="img/nav-bar/100rodeios.png" alt=""></a></div>
+            <div><a href="home.php"><img src="img/nav-bar/100rodeios.png" alt=""></a></div>
             <div><a href="#"><img src="img/nav-bar/lupa.png" alt=""></a></div>
         </div>
         <!-- <div class=" "><img class="uk-position-bottom linhas" src="img/nav-bar/linhas.png" alt=""></div> -->
     </div>
-    <div class="uk-position-relative">
+    <!-- <div class="uk-position-relative">
         <img class="onda-azul-topo uk-position-top" src="img/nav-bar/onda-topo-azul.png" alt="">
-    </div>
+    </div> -->
 </div>
 
 <?php require_once("menu-lateral.php")?>
